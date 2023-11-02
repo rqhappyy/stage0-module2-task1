8a3beec514aa3548cbd8c8e5b6ab8b67a7940526
rqhappyy <dtereschenko110@gmail.com>
2023-11-02 11:59:12 +0300

    added print in console

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -1,7 +1,6 @@
 package com.mjc.stage0;
 
 public class FirstApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {System.out.println("Hello, World!");
     }
 }
